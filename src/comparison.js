63f6215669ba36b3916dff87e5092f92b6fab90b
Petr Romanov <MucTop@yandex.ru>
2023-03-01 18:42:50 +0300

    update3

--- src/comparison.js
+++ src/comparison.js
@@ -1,47 +1,60 @@
 import _ from 'lodash';
 
+const isObject = (obj) => _.isObject(obj) && !_.isArray(obj);
+
+const getAdded = (key, value) => ({
+  key,
+  value,
+  type: 'added',
+});
+
+const getDeleted = (key, value) => ({
+  key,
+  value,
+  type: 'deleted',
+});
+
+const getChanged = (key, valueBefore, valueAfter) => ({
+  key,
+  valueBefore,
+  valueAfter,
+  type: 'changed',
+});
+
+const getNested = (key, children) => ({
+  key,
+  type: 'nested',
+  children,
+});
+
+const getUnchanged = (key, value) => ({
+  key,
+  value,
+  type: 'unchanged',
+});
+
 const comparison = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
   const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
-  const result = sortedKeys.map((key) => {
+  const results = [];
+
+  sortedKeys.forEach((key) => {
     if (!_.has(obj1, key)) {
-      return {
-        key,
-        value: obj2[key],
-        type: 'added',
-      };
+      results.push(getAdded(key, obj2[key]));
+    } else if (!_.has(obj2, key)) {
+      results.push(getDeleted(key, obj1[key]));
+    } else if (isObject(obj1[key]) && isObject(obj2[key])) {
+      results.push(getNested(key, comparison(obj1[key], obj2[key])));
+    } else if (obj1[key] !== obj2[key]) {
+      results.push(getChanged(key, obj1[key], obj2[key]));
+    } else {
+      results.push(getUnchanged(key, obj1[key]));
     }
-    if (!_.has(obj2, key)) {
-      return {
-        key,
-        value: obj1[key],
-        type: 'deleted',
-      };
-    }
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return {
-        key,
-        type: 'nested',
-        children: comparison(obj1[key], obj2[key]),
-      };
-    }
-    if (obj1[key] !== obj2[key]) {
-      return {
-        key,
-        valueBefore: obj1[key],
-        valueAfter: obj2[key],
-        type: 'changed',
-      };
-    }
-    return {
-      key,
-      value: obj1[key],
-      type: 'unchanged',
-    };
   });
-  return result;
+
+  return results;
 };
 
 export default comparison;
